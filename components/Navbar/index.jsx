91313630d9ed30d5764a3d788f7791bad06cd2c3
Ashutosh Maurya <ashutoshm9695@gmail.com>
2023-01-01 16:16:49 +0530

    deal of the day section

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -10,174 +10,168 @@ const Navbar = () => {
   const [searchItem, setSearchItem] = useState();
 
   return (
-    <Box>
-      <div className="bg-white ">
-        <div className="border px-6 ">
-          <div className="flex justify-between items-center h-18">
-            {/* toggle button */}
-            <button
-              data-collapse-toggle="navbar-default"
-              type="button"
-              className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-default"
-              aria-expanded="false"
+    <div className="sticky top-0 z-50 bg-white shadow-md w-full">
+      <div className="border px-6 ">
+        <div className="flex justify-between items-center h-18">
+          {/* toggle button */}
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-6 h-6"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </button>
+              <path
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
 
-            {/* items section */}
-            <div
-              className="flex items-center hidden w-full md:flex md:w-auto "
-              id="navbar-default"
-            >
-              <Link href="/" className="transition duration-300 ease-in-out ">
-                <Image
-                  height="15"
-                  width="50"
-                  className="mx-8  md:mx-4"
-                  src={logo}
-                  alt="logo"
-                />
-              </Link>
-
-              <div className="flex gap-x-6 md:gap-x-3">
-                <span className="cursor-pointer text-gray-800  py-7 px-1 text-sm font-medium ">
-                  MEN
-                </span>
-                <span className="cursor-pointer text-gray-800 py-7 px-1 text-sm font-medium hover:border-b-4 border-red-500">
-                  WOMEN
-                </span>
-                <span className="cursor-pointer text-gray-800 py-7 px-1 text-sm font-medium hover:border-b-4 border-red-500">
-                  KIDS
-                </span>
-                <span className="cursor-pointer text-gray-800 py-7 px-1 text-sm font-medium hover:border-b-4 border-red-500">
-                  HOME & LIVING
-                </span>
-                <span className="cursor-pointer text-gray-800 py-7 px-1 text-sm font-medium hover:border-b-4 border-red-500">
-                  BEAUTY
-                </span>
-                <span className="cursor-pointer hidden w-full md:block md:w-auto text-gray-800 py-7 px-1 text-sm font-medium hover:border-b-4 border-red-500">
-                  STUDIO
-                </span>
-              </div>
-            </div>
-
-            {/* search section */}
-            <div className="ml-6  hidden md:block ">
-              <input
-                type="text"
-                className="from-control  md:w-80 bg-gray-50  w-full rounded-md border border-[#DDE2E4] px-3 py-2 text-sm outline-none focus:border-gray-600"
-                value={searchItem}
-                onChange={(e) => setSearchItem(e.target.value)}
-                placeholder="Search for products, brands & more"
+          {/* items section */}
+          <div
+            className="flex items-center hidden w-full md:flex md:w-auto "
+            id="navbar-default"
+          >
+            <Link href="/" className="transition duration-300 ease-in-out ">
+              <Image
+                height="15"
+                width="50"
+                className="mx-8  md:mx-4"
+                src={logo}
+                alt="logo"
               />
+            </Link>
+
+            <div className="flex gap-x-6 md:gap-x-3">
+              <span className="cursor-pointer text-gray-800  py-7 px-1 text-sm font-medium ">
+                MEN
+              </span>
+              <span className="cursor-pointer text-gray-800 py-7 px-1 text-sm font-medium hover:border-b-4 border-red-500">
+                WOMEN
+              </span>
+              <span className="cursor-pointer text-gray-800 py-7 px-1 text-sm font-medium hover:border-b-4 border-red-500">
+                KIDS
+              </span>
+              <span className="cursor-pointer text-gray-800 py-7 px-1 text-sm font-medium hover:border-b-4 border-red-500">
+                HOME & LIVING
+              </span>
+              <span className="cursor-pointer text-gray-800 py-7 px-1 text-sm font-medium hover:border-b-4 border-red-500">
+                BEAUTY
+              </span>
+              <span className="cursor-pointer hidden w-full md:block md:w-auto text-gray-800 py-7 px-1 text-sm font-medium hover:border-b-4 border-red-500">
+                STUDIO
+              </span>
             </div>
+          </div>
 
-            {/* account section */}
-            <div className="ml-2 flex ">
-              <div className="group inline-block relative flex flex-col items-center cursor-pointer  px-2 py-6 hover:border-b-4 border-red-500">
-                <img
-                  className="h3 w-4"
-                  src="https://cdn-icons-png.flaticon.com/512/1250/1250689.png"
-                />
+          {/* search section */}
+          <div className="ml-6  hidden md:block ">
+            <input
+              type="text"
+              className="from-control  md:w-80 bg-gray-50  w-full rounded-md border border-[#DDE2E4] px-3 py-2 text-sm outline-none focus:border-gray-600"
+              value={searchItem}
+              onChange={(e) => setSearchItem(e.target.value)}
+              placeholder="Search for products, brands & more"
+            />
+          </div>
 
-                <span className="text-xs font-medium">Profile</span>
+          {/* account section */}
+          <div className="ml-2 flex ">
+            <div className="group inline-block relative flex flex-col items-center cursor-pointer  px-2 py-6 hover:border-b-4 border-red-500">
+              <img
+                className="h3 w-4"
+                src="https://cdn-icons-png.flaticon.com/512/1250/1250689.png"
+              />
 
-                <div className="absolute w-72 p-5 shadow-lg hidden text-gray-700 mt-16 z-10 bg-white group-hover:block">
-                  <div className="">
-                    <div className="text-sm font-semibold">Welcome</div>
-                    <div className="text-sm ">
-                      To access account and manage orders
-                    </div>
+              <span className="text-xs font-medium">Profile</span>
 
-                    <Link
-                      href="/login"
-                      className="  transition duration-300 ease-in-out "
-                    >
-                      {" "}
-                      <button className="bg-transparent hover:border-red-600 block text-black  py-1 px-2 border border-red-400  rounded">
-                        LOGIN / SIGNUP
-                      </button>
-                    </Link>
-                    <hr className="my-4" />
+              <div className="absolute w-72 p-5 shadow-lg hidden text-gray-700 mt-16 z-10 bg-white group-hover:block">
+                <div className="">
+                  <div className="text-sm font-semibold">Welcome</div>
+                  <div className="text-sm ">
+                    To access account and manage orders
+                  </div>
 
-                    <div>
-                      <div className="text-sm hover:font-semibold">Orders </div>
-                      <div className="text-sm hover:font-semibold">
-                        Wishlist{" "}
-                      </div>
-                      <div className="text-sm hover:font-semibold">
-                        Gift Cards{" "}
-                      </div>
-                      <div className="text-sm hover:font-semibold">
-                        Contact Us{" "}
-                      </div>
-                      <div className="">
-                        <span className="text-sm hover:font-semibold">
-                          Myntra Insider{" "}
-                        </span>
-                        <span className="items-center justify-center bg-red-400 px-2 text-xs text-white">
-                          new
-                        </span>
-                      </div>
-                    </div>
+                  <Link
+                    href="/login"
+                    className="  transition duration-300 ease-in-out "
+                  >
+                    {" "}
+                    <button className="bg-transparent hover:border-red-600 block text-black  py-1 px-2 border border-red-400  rounded">
+                      LOGIN / SIGNUP
+                    </button>
+                  </Link>
+                  <hr className="my-4" />
 
-                    <hr className="my-4" />
-
+                  <div>
+                    <div className="text-sm hover:font-semibold">Orders </div>
+                    <div className="text-sm hover:font-semibold">Wishlist </div>
                     <div className="text-sm hover:font-semibold">
-                      Myntra Credit{" "}
-                    </div>
-                    <div className="text-sm hover:font-semibold">Coupons </div>
-                    <div className="text-sm hover:font-semibold">
-                      Saved Cards{" "}
+                      Gift Cards{" "}
                     </div>
                     <div className="text-sm hover:font-semibold">
-                      Saved VPA{" "}
+                      Contact Us{" "}
                     </div>
-                    <div className="text-sm hover:font-semibold">
-                      Saved Addresses
+                    <div className="">
+                      <span className="text-sm hover:font-semibold">
+                        Myntra Insider{" "}
+                      </span>
+                      <span className="items-center justify-center bg-red-400 px-2 text-xs text-white">
+                        new
+                      </span>
                     </div>
+                  </div>
+
+                  <hr className="my-4" />
+
+                  <div className="text-sm hover:font-semibold">
+                    Myntra Credit{" "}
+                  </div>
+                  <div className="text-sm hover:font-semibold">Coupons </div>
+                  <div className="text-sm hover:font-semibold">
+                    Saved Cards{" "}
+                  </div>
+                  <div className="text-sm hover:font-semibold">Saved VPA </div>
+                  <div className="text-sm hover:font-semibold">
+                    Saved Addresses
                   </div>
                 </div>
               </div>
+            </div>
 
-              <div className="flex flex-col items-center cursor-pointer   rounded-md py-6 px-2 ">
+            <div className="flex flex-col items-center cursor-pointer   rounded-md py-6 px-2 ">
+              <img
+                className="h3 w-4"
+                src="https://cdn-icons-png.flaticon.com/512/445/445140.png"
+              />
+              <span className="text-xs font-medium">Wishlist</span>
+            </div>
+
+            <div className="flex flex-col items-center cursor-pointer   rounded-md py-6 px-2 ">
+              <div className="relative">
                 <img
                   className="h3 w-4"
-                  src="https://cdn-icons-png.flaticon.com/512/445/445140.png"
+                  src="https://img.icons8.com/fluency-systems-regular/48/null/shopping-bag--v1.png"
                 />
-                <span className="text-xs font-medium">Wishlist</span>
-              </div>
 
-              <div className="flex flex-col items-center cursor-pointer   rounded-md py-6 px-2 ">
-                <div className="relative">
-                  <img
-                    className="h3 w-4"
-                    src="https://img.icons8.com/fluency-systems-regular/48/null/shopping-bag--v1.png"
-                  />
-
-                  {/* <span className="absolute -top-2 -right-2 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 p-2 text-xs text-white">3</span> */}
-                </div>
-                <span className="text-xs font-medium">Bag</span>
+                {/* <span className="absolute -top-2 -right-2 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 p-2 text-xs text-white">3</span> */}
               </div>
+              <span className="text-xs font-medium">Bag</span>
             </div>
           </div>
         </div>
       </div>
-    </Box>
+    </div>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,192 @@
 import { Inter } from "@next/font/google";
 import Carousel from "../components/Carousel";
-import Dropdown from "../components/Dropdown";
-import styles from "../styles/Home.module.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
+// deal-od-day
+import dealOne from "../public/static/deal-of-day/deal-of-day-1.png";
+import dealTwo from "../public/static/deal-of-day/deal-of-day-2.png";
+import dealThree from "../public/static/deal-of-day/deal-of-day-3.png";
+import dealFour from "../public/static/deal-of-day/deal-of-day-4.png";
+import dealFive from "../public/static/deal-of-day/deal-of-day-5.png";
+import dealSix from "../public/static/deal-of-day/deal-of-day-6.png";
+import dealSeven from "../public/static/deal-of-day/deal-of-day-7.png";
+import dealEight from "../public/static/deal-of-day/deal-of-day-8.png";
+
+// exclusive-brand
+import exclusiveOne from "../public/static/exclusive-brand/exclusive-brand-1.png";
+import exclusiveTwo from "../public/static/exclusive-brand/exclusive-brand-2.png";
+import exclusiveThree from "../public/static/exclusive-brand/exclusive-brand-3.png";
+import exclusiveFour from "../public/static/exclusive-brand/exclusive-brand-4.png";
+import exclusiveFive from "../public/static/exclusive-brand/exclusive-brand-5.png";
+import exclusiveSix from "../public/static/exclusive-brand/exclusive-brand-6.png";
+import exclusiveSeven from "../public/static/exclusive-brand/exclusive-brand-7.png";
+import exclusiveEight from "../public/static/exclusive-brand/exclusive-brand-8.png";
 
 export default function Home() {
   return (
     <>
       <Carousel />
+      <div>
+        <div className="ml-0 px-8 mt-20 mb-14 text-3xl text-gray-700 font-semibold tracking-widest ">
+          DEAL OF THE DAY
+        </div>
+        <div className="flex xs:flex-wrap xs:justify-center w-full">
+          <Image
+            src={dealOne}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={dealTwo}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={dealThree}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={dealFour}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={dealFive}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={dealSix}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={dealSeven}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={dealEight}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+        </div>
+      </div>
+      <div>
+        <div className="ml-0 px-8 m-14 text-3xl text-gray-700 font-semibold tracking-widest ">
+          BEST OF MYNTRA EXCLUSIVE BRANDS
+        </div>
+        <div className="flex gap-1 md:flex-wrap md:justify-center w-full">
+          <Image
+            src={exclusiveOne}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={exclusiveTwo}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={exclusiveThree}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={exclusiveFour}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={exclusiveFive}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={exclusiveSix}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={exclusiveSeven}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={exclusiveEight}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+        </div>
 
-      <div className="ml-0 px-10 mt-28 text-4xl text-gray-700   font-semibold tracking-widest ">
-        DEAL OF THE DAY
+        <div className="flex gap-1 xs:flex-wrap xs:justify-center w-full">
+          <Image
+            src={dealOne}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={dealTwo}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={dealThree}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={dealFour}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={dealFive}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={dealSix}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={dealSeven}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+          <Image
+            src={dealEight}
+            alt="deal-of-the-day"
+            height={250}
+            width="auto"
+          />
+        </div>
       </div>
     </>
   );
