d8c46309aa6f210e189a0f265427a3351c762122
Ashutosh Maurya <ashutoshm9695@gmail.com>
2023-01-08 19:16:13 +0530

    offer

--- pages/profile/Coupons.jsx
+++ pages/profile/Coupons.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Coupons = () => {
-  return <div>Coupons</div>;
+  return (
+    <div>
+      <div className="border border-gray-300x">
+        <div>
+          <div>
+            <p>150</p>
+            <p>OFF</p>
+          </div>
+          <div>
+            <p>On minimum purchase of Rs. 0</p>
+            <span>CODE: </span> <span>B2GET50</span>
+          </div>
+        </div>
+
+        <div>
+          <div>Expiry: JAN 09 2023 | 11:59:00 P.M</div>
+          <div>Details</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Coupons;
