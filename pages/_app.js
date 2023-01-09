6d769599260de8e351ff245ecbd0f998e3f19c07
Ashutosh Maurya <ashutoshm9695@gmail.com>
2023-01-10 01:30:50 +0530

    admin off

--- pages/_app.js
+++ pages/_app.js
@@ -6,25 +6,24 @@ import { AdminLayout } from "../layouts";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-   const isAdmin = true;
-   return (
-      <Fragment>
-         {isAdmin ? (
-            <AdminLayout>
-               <Component {...pageProps} />{" "}
-            </AdminLayout>
-         ) : (
-            <Fragment>
-               <Head>
-                  <title>
-                     Online Shopping for Women, Men, Kids Fashion & Lifestyle -
-                     Myntra
-                  </title>
-               </Head>
-               <Navbar />
-               <Component {...pageProps} />
-            </Fragment>
-         )}
-      </Fragment>
-   );
+  const isAdmin = false;
+  return (
+    <Fragment>
+      {isAdmin ? (
+        <AdminLayout>
+          <Component {...pageProps} />{" "}
+        </AdminLayout>
+      ) : (
+        <Fragment>
+          <Head>
+            <title>
+              Online Shopping for Women, Men, Kids Fashion & Lifestyle - Myntra
+            </title>
+          </Head>
+          <Navbar />
+          <Component {...pageProps} />
+        </Fragment>
+      )}
+    </Fragment>
+  );
 }
