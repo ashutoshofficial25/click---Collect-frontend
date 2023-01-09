1d815cc5bac04b3397b11bd4ee50adc2ef32a2cd
Ashutosh Maurya <ashutoshm9695@gmail.com>
2023-01-09 13:51:43 +0530

    ..

--- layouts/AdminLayout/components/Header/index.jsx
+++ layouts/AdminLayout/components/Header/index.jsx
@@ -1,5 +1,76 @@
-const Header = () => {
-   return <div>Header</div>;
-};
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import IconButton from '@mui/material/IconButton'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import InputAdornment from '@mui/material/InputAdornment'
 
-export default Header;
+// ** Icons Imports
+import MenuIcon from '@mui/icons-material/Menu'
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+
+// ** Type Import
+
+// ** Components
+// import ModeToggler from 'src/@core/layouts/components/shared-components/ModeToggler'
+// import UserDropdown from 'src/@core/layouts/components/shared-components/UserDropdown'
+// import NotificationDropdown from 'src/@core/layouts/components/shared-components/NotificationDropdown'
+
+
+
+const Header = (props) => {
+  // ** Props
+  const { hidden, settings, saveSettings, toggleNavVisibility } = props
+
+  // ** Hook
+
+
+  return (
+    <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+      <Box className='actions-left' sx={{ mr: 2, display: 'flex', alignItems: 'center' }}>
+        {hidden ? (
+          <IconButton
+            color='inherit'
+            // onClick={toggleNavVisibility}
+            // sx={{ ml: -2.75, ...(hiddenSm ? {} : { mr: 3.5 }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+        ) : null}
+        <TextField
+          size='small'
+          sx={{ '& .MuiOutlinedInput-root': { borderRadius: 4 } }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <ManageSearchIcon fontSize='small' />
+              </InputAdornment>
+            )
+          }}
+        />
+      </Box>
+      <Box className='actions-right' sx={{ display: 'flex', alignItems: 'center' }}>
+        { (
+          <Box
+            component='a'
+            target='_blank'
+            rel='noreferrer'
+            sx={{ mr: 4, display: 'flex' }}
+            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free'
+          >
+            <img
+              height={24}
+              alt='github stars'
+              src='https://img.shields.io/github/stars/themeselection/materio-mui-react-nextjs-admin-template-free?style=social'
+            />
+          </Box>
+        )}
+        {/* <ModeToggler settings={settings} saveSettings={saveSettings} />
+        <NotificationDropdown />
+        <UserDropdown /> */}
+      </Box>
+    </Box>
+  )
+}
+
+export default Header
