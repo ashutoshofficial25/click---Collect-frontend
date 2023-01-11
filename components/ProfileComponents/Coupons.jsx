a6c4d554768394454baf3ebeb7a62aa6c79503a4
Ashutosh Maurya <ashutoshm9695@gmail.com>
2023-01-11 23:19:36 +0530

    ..

--- components/ProfileComponents/Coupons.jsx
+++ components/ProfileComponents/Coupons.jsx
@@ -1,25 +1,64 @@
 import React from "react";
 
+const offers = [
+  {
+    discount: "150",
+    code: "B2GET50",
+    minPurchaseValue: "0",
+    expireDate: "JAN 09 2013",
+    expireTime: "11:50:00 P.M",
+  },
+  {
+    discount: "300",
+    code: "PE300",
+    minPurchaseValue: "1,999",
+    expireDate: "JAN 31 2013",
+    expireTime: "11:50:00 P.M",
+  },
+  {
+    discount: "2000",
+    code: "PREMIUM2KOFF",
+    minPurchaseValue: "6,999",
+    expireDate: "JAN 16 2013",
+    expireTime: "11:50:00 P.M",
+  },
+  {
+    discount: "30%",
+    code: "COLLECTIVE30",
+    minPurchaseValue: "0",
+    expireDate: "JAN 15 2013",
+    expireTime: "11:50:00 P.M",
+  },
+];
+
 const Coupons = () => {
   return (
     <div>
-      <div className="border border-gray-300x">
-        <div>
-          <div>
-            <p>150</p>
-            <p>OFF</p>
+      {offers.map((offer) => (
+        <div className="border border-gray-300x mt-5">
+          <div className="flex items-center">
+            <div className="w-fit px-10 font-bold text-emerald-500 py-5  border-r-2">
+              <div>{offer.discount}</div>
+              <div>OFF</div>
+            </div>
+            <div className="text-sm text-gray-500 px-5">
+              <p>On minimum purchase of Rs. {offer.minPurchaseValue}</p>
+              <span>CODE: </span> <span>{offer.code}</span>
+            </div>
           </div>
-          <div>
-            <p>On minimum purchase of Rs. 0</p>
-            <span>CODE: </span> <span>B2GET50</span>
-          </div>
-        </div>
 
-        <div>
-          <div>Expiry: JAN 09 2023 | 11:59:00 P.M</div>
-          <div>Details</div>
+          <hr className="mx-5" />
+          <div className="flex text-sm text-gray-500 justify-between px-5 py-4">
+            <div>
+              Expiry: {offer.expireDate} |{" "}
+              <span className="font-bold"> {offer.expireTime}</span>{" "}
+            </div>
+            <div className="font-bold text-indigo-400 cursor-pointer">
+              Details
+            </div>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
