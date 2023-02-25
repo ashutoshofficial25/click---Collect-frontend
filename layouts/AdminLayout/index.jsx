c4fcd68cae3a4e6e7e88109d104c756e7822376b
Ashutosh Maurya <ashutoshm9695@gmail.com>
2023-02-25 17:12:41 +0530

    ..

--- layouts/AdminLayout/index.jsx
+++ layouts/AdminLayout/index.jsx
@@ -5,13 +5,16 @@ import Dashborad from "../../pages/admin/Dashborad";
 const AdminLayout = ({ children }) => {
   const [isMobileNavOpen, setMobileNavOpen] = useState(false);
   return (
-    <main class="min-h-screen w-full bg-gray-100 text-gray-700" x-data="layout">
+    <main
+      className="min-h-screen w-full bg-gray-100 text-gray-700"
+      x-data="layout"
+    >
       <Header />
 
-      <div class="flex">
+      <div className="flex">
         <Sidebar />
 
-        <div class="w-full p-4">
+        <div className="w-full p-4">
           <Dashborad />
         </div>
       </div>
