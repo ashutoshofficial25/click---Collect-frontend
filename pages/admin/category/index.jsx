3be5bb9f2faa03c70a0741f820b7e23ec6dfe560
Ashutosh Maurya <dev26@bytescrum.com>
2023-03-15 10:35:01 +0530

    category and payment section

---
+++ pages/admin/category/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Category = () => {
+  return (
+    <div className="text-rose-500">
+      hello these are the following categories
+    </div>
+  );
+};
+export default Category;
---
+++ pages/admin/payment/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Payment = () => {
+  return <div className="text-rose-500">hello payemtns</div>;
+};
+export default Payment;
